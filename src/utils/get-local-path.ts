f04e7418b8b14b211c9a9391cac2890770c8d5ca
Paul Popat <p.t.popat@googlemail.com>
2020-09-03 15:16:15 +0100

    Fixed development issues

--- src/utils/get-local-path.ts
+++ src/utils/get-local-path.ts
@@ -3,8 +3,8 @@ import Path from "path";
 
 export function GetAppContentPath(path: string) {
   if (process.env.IS_DEV) {
-    return Path.join(app.getAppPath(), path);
+    return "./" + path;
   }
 
-  return "./" + path;
+  return Path.join(app.getAppPath(), path);
 }
